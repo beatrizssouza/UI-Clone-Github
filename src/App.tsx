3e5d886140bce9e2114d77f2f4082d285be52f44
beatrizssouza <msfemacell@gmail.com>
2021-01-23 17:17:49 -0300

    criação das pages profile e Repo, criação dos components header,profileCard,RepoCard

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import 'react-calendar-heatmap/dist/styles.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header/index';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
+function App() {
+  return (  
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />}/>
+      </Routes>
+
+      <GlobalStyles />
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Container, Main, LeftSide, RightSide, Repos } from './styles';
+import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
+
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <Main>
+
+        <LeftSide> 
+          <ProfileData 
+            username={'Beatrizssouza'}
+            name={'Beatriz Silva Souza'}
+            avatarUrl ={'https://avatars.githubusercontent.com/u/49240794?v=4'}
+            followers={20}
+            following={5}
+            company={'VipTech'}
+            location={'Campo Mourão, Brazil'}
+            email={'beatrizssouza00@gmail.com'}
+            blog={'https://www.linkedin.com/in/beatriz-silva-a96a07187/'}
+          />
+        </LeftSide>
+
+        <RightSide>
+          <Repos>
+            <h2>Random repos</h2>
+
+            <div>
+              {[1,2,3,4,5,6].map(n => (
+                <RepoCard 
+                  key={n}
+                  username={'beatrizssouza'}
+                  reponame={'covid-19'}
+                  description={'Contains all of my page covid lessons code'}
+                  language={n % 3 == 0 ? 'Javascript': 'TypeScript'}
+                  stars={8}
+                  forks={4}
+                  />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
+      
+      </Main>
+    </Container>
+  );
+}
+
+export default Profile;
